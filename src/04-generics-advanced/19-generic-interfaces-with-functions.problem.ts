906ad8d4f3542fc192f3fd3727008bb82712f5fd
adrianhm13 <adrian.hervasmartinez@gmail.com>
2024-02-08 20:37:30 +0100

    adds solution 04.19
    generic interfaces with functions

--- src/04-generics-advanced/19-generic-interfaces-with-functions.problem.ts
+++ src/04-generics-advanced/19-generic-interfaces-with-functions.problem.ts
@@ -5,7 +5,7 @@ export interface Cache<T> {
   get: (key: string) => T | undefined;
   set: (key: string, value: T) => void;
   // You can fix this by only changing the line below!
-  clone: (transform: (elem: unknown) => unknown) => Cache<unknown>;
+  clone: <U>(transform: (elem: T) => U) => Cache<U>;
 }
 
 const createCache = <T>(initialCache?: Record<string, T>): Cache<T> => {
@@ -48,7 +48,6 @@ it("Should let you clone the cache using a transform function", () => {
   });
 
   const a = stringCache.get("a");
-
   expect(a).toEqual("1");
 
   type tests = [Expect<Equal<typeof a, string | undefined>>];
